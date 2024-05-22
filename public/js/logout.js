c818a5a8e4379c1c9e73019e98797e9166fea616
AliyusUnderwood <158104889+AliyusUnderwood@users.noreply.github.com>
2024-05-21 21:57:56 -0700

    Fixed login, logout, and signup

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,7 @@
-const logout = async () => {
-    const response = await fetch('/logout', {
+
+    console.log("logout route")
+    const logout = async () => {
+    const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
@@ -13,4 +15,7 @@ const logout = async () => {
   
 
 
-document.querySelector('#logout').addEventListener('click', logout);
+  document.querySelector('#logout').addEventListener('click', (event) => {
+    event.preventDefault();
+    logout();
+  });
